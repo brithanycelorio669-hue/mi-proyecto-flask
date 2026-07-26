2547bddf53e00d8da3bd831f846c93336ffcd78d
Brithany Celorio <brithanycelorio669@gmail.com>
2026-07-26 15:13:42 -0500

    Actualización del proyecto

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return '<h1>Mi Aplicación Flask</h1><p>Bienvenido a mi proyecto</p>'
-
+    return '<h1>Mi Aplicación Flask</h1><p>Bienvenido a mi proyecto Flask actualizado con Git!</p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
